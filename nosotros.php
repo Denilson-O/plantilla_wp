d714d72bad6197b49d3aadbb55d88d2ab71dcddd
cristhian160220 <jhareedo@gmail.com>
2024-05-16 09:47:04 -0500

    actualizado

--- nosotros.php
+++ nosotros.php
@@ -24,14 +24,39 @@
 </style>
 <body>
 <?php include('include/menu.php'); ?>
-    <br><br><br><div class="container my-5">
-        <div class="row">
+
+<div class="main-content">
+
+                <div class="page-content">
+                    <div class="container-fluid">
+
+                        <!-- start page title -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="page-title-box d-flex align-items-center justify-content-between">
+                                    <h4 class="mb-0 font-size-18">Nosotros</h4>
+
+                                    <div class="page-title-right">
+                                        <ol class="breadcrumb m-0">
+                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Inicio</a></li>
+                                            <li class="breadcrumb-item active">Nosotros</li>
+                                        </ol>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end page title -->
+
+                      
+                        <div class="row">
             <div class="col-md-4 d-flex justify-content-center align-items-center">
             <img src="img/nos.jpeg" class="img-fluid" alt="img">
 
             </div>
             <div class="col-md-8" style="text-align: justify;">
-                <h2><center>Sobre Nosotros</center></h2>
+                <h1 style="color:orange"><center>Sobre<span style="color:red"> Nosotros</span></center></h1>
+                <hr>
                
                 <p>
                     Bienvenidos a nuestra página web. Somos una empresa dedicada a brindar la compra yu venta de autos lujos y deportivos.
@@ -40,6 +65,11 @@
                     Nos enorgullece ofrecer productos y servicios de alta calidad que superan las expectativas de
                     nuestros clientes.
                 </p>
+                <p>En nuestra empresa, nos dedicamos a ofrecer una experiencia única en la compra y venta de autos de lujo y deportivos. Nuestro compromiso es brindar a nuestros clientes un servicio excepcional y productos de la más alta calidad.
+
+               Contamos con un equipo de profesionales altamente calificados y apasionados por el mundo automotriz, quienes están comprometidos en superar las expectativas de nuestros clientes en cada transacción.
+
+                 Nuestra misión es revolucionar el mercado de autos de lujo y deportivos en el Perú, ofreciendo un enfoque innovador y personalizado. Nos esforzamos por crear relaciones a largo plazo con nuestros clientes, basadas en la confianza y la transparencia.</p>
                 <p>
                     Nuestra misión es mejorar y revolucionar el concepto de compra y venta de autos de lujo seminuevos y autos deportivos en el Perú bajo un esquema innovador y personalizado desde la comodidad de tu hogar.
 
@@ -50,9 +80,207 @@
                     Gracias por visitarnos. Esperamos poder trabajar con ustedes y ayudarlos a alcanzar sus objetivos.
                 </p>
             </div>
+            
         </div>
-        <?php include('include/pie_pagina.php'); ?>
     </div>
+    <div class="row mt-5">
+    <div class="col-md-12">
+    <h2 style="color:orange"><center>Nuestro<span style="color:red"> Equipo</span></center></h2>
+        <hr>
+        <div class="container px-3 px-0 mb-5 contentDiv">
+
+    <div class="row pt-3 pb-md-3 mt-3">
+
+        <div class="row px-0 mx-auto">
+
+            <div class="mx-auto mt-md-0">
+
+                <div class="row">
+
+
+                    <!-- Mateo Kahatt  -->
+                    <div class="col-md-4">
+
+                        <div class="card order-1 shadow-2-strong ">
+                            <!-- Card image -->
+                            <img class="card-img-top w-100 h-auto" src="img/preguntas_frecuentes.png" alt="Card image cap">
+                            <a>
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                            <!-- Card content -->
+                            <div class="card-body">
+                                <!-- Title -->
+                                <h4 class="card-title" style="text-align: center"><strong>Mateo Kahatt</strong></h4>
+                                <!-- Subtitle -->
+                                <h6 class="fw-bold indigo-text py-2" style="text-align: center">CEO</h6>
+                                <!-- Linkedin -->
+                                
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <!-- Gabriel Delgado  -->
+                    <div class="col-md-4 mt-5 mt-md-0">
+
+                        <div class="card order-0 shadow-2-strong ">
+                            <!-- Card image -->
+                            <img class="card-img-top w-100 h-auto" src="img/GD.png" alt="Card image cap">
+                            <a>
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                            <!-- Card content -->
+                            <div class="card-body">
+                                <!-- Title -->
+                                <h4 class="card-title" style="text-align: center"><strong>Gabriel Delgado</strong></h4>
+                                <!-- Subtitle -->
+                                <h6 class="fw-bold indigo-text py-2" style="text-align: center">Director General</h6>
+                                <!-- Linkedin -->
+                                
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <!-- Juan Ayllon  -->
+
+                    <div class="col-md-4 mt-5 mt-md-0">
+
+                        <div class="card shadow-2-strong ">
+                            <!-- Card image -->
+                            <img class="card-img-top w-100 h-auto" src="img/JA.png" alt="Card image cap">
+                            <a>
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                            <!-- Card content -->
+                            <div class="card-body">
+                                <!-- Title -->
+                                <h4 class="card-title" style="text-align: center"><strong>Juan Ayllon</strong></h4>
+                                <!-- Subtitle -->
+                                <h6 class="fw-bold indigo-text py-2" style="text-align: center">Inspección Vehicular</h6>
+
+                                
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="row px-0 mx-auto pt-5">
+
+            <div class="mx-auto mt-md-0">
+
+                <div class="row">
+
+                    <!-- Diego Encinas  -->
+                    <div class="col-md-4">
+
+                        <div class="card shadow-2-strong ">
+                            <!-- Card image -->
+                            <img class="card-img-top w-100 h-auto" src="img/DE2 (1).png" alt="Card image cap">
+                            <a>
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                            <!-- Card content -->
+                            <div class="card-body">
+                                <!-- Title -->
+                                <h4 class="card-title" style="text-align: center"><strong>Diego Encinas</strong></h4>
+                                <!-- Subtitle -->
+                                <h6 class="fw-bold indigo-text py-2" style="text-align: center">Asesor Comercial</h6>
+
+                                
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <!-- Giacomo Garibaldi  -->
+                    <div class="col-md-4 mt-5 mt-md-0">
+
+                        <div class="card shadow-2-strong ">
+                            <!-- Card image -->
+                            <img class="card-img-top w-100 h-auto" src="img/GG.png" alt="Card image cap">
+                            <a>
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                            <!-- Card content -->
+                            <div class="card-body">
+                                <!-- Title -->
+                                <h4 class="card-title" style="text-align: center"><strong>Giacomo Garibaldi</strong></h4>
+                                <!-- Subtitle -->
+                                <h6 class="fw-bold indigo-text py-2" style="text-align: center">Branding | Asesor Comercial</h6>
+
+                               
+                            </div>
+                        </div>
+
+                    </div>
+
+                
+
+                    <!-- Alvaro Cauper  -->
+                    <div class="col-md-4 mt-5 mt-md-0">
+
+                        <div class="card shadow-2-strong ">
+                            <!-- Card image -->
+                            <img class="card-img-top w-100 h-auto" src="img/DE.png" alt="Card image cap">
+                            <a>
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                            <!-- Card content -->
+                            <div class="card-body">
+                                <!-- Title -->
+                                <h4 class="card-title" style="text-align: center"><strong>Alvaro Cauper</strong></h4>
+                                <!-- Subtitle -->
+                                <h6 class="fw-bold indigo-text py-2" style="text-align: center">Audiovisual | Asesor Comercial</h6>
+
+                               
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        
+
+
+    </div>
+
+</div>
+            <!-- Add more team members here -->
+        </div>
+    </div>
+</div>
+    
+                    </div> <!-- container-fluid -->
+                </div>
+                <!-- End Page-content -->
+                <?php
+include("include/pie_pagina.php");
+?>
+                
+
+            </div>
+            <!-- end main content-->
+
+
+
+</div>
+
 
 
 
